5e452fe08f509c7b465b8239a9893f43effbe219
Brian Bunker <bbunk001@ucr.edu>
2023-09-19 09:04:19 -0700

    finished semantics

--- capstone/src/Footer.js
+++ capstone/src/Footer.js
@@ -1,6 +1,21 @@
 function Footer() {
     return (
-        <></>
+        <footer>
+            <img></img>
+            <h4>Doormat Navigation</h4>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <h4>Contact</h4>
+            <ul>
+                <li></li>
+            </ul>
+            <h4>Social Media Links</h4>
+            
+        </footer>
     )
 }
 
