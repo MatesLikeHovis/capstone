d054c7feacdeebff9ced4a5d2a5f0e9e5fa08f52
Brian Bunker <bbunk001@ucr.edu>
2023-09-20 14:27:33 -0700

    modified:   src/App.css 	modified:   src/Hero.js

--- capstone/src/Hero.js
+++ capstone/src/Hero.js
@@ -1,6 +1,13 @@
 function Hero() {
     return (
-        <section className="Hero"><p>The Hero Section</p>
+        <section className="Hero">
+            <div className="TitleWrapper">
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do quismod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <button className="ReserveTable stdButton">Reserve a Table</button>
+            <img className="heroImage" src="/restauranfood.jpg" alt="Bruschetta being delivered by a waiter"></img>
         </section>
     )    
 }
