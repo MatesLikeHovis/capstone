47a8c0415bb8ca7b7a6068c5affa83a5aaafa0da
Brian Bunker <bbunk001@ucr.edu>
2023-09-20 09:36:53 -0700

    modified:   src/App.js 	modified:   src/Main.js

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -2,10 +2,7 @@ import './App.css';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import Hero from './Hero';
-import Specials from './Specials';
-import Testimonials from './Testimonials';
-import About from './About';
+
 
 function App() {
   return (
--- capstone/src/Main.js
+++ capstone/src/Main.js
@@ -1,3 +1,8 @@
+import Hero from './Hero';
+import Specials from './Specials';
+import Testimonials from './Testimonials';
+import About from './About';
+
 function Main() {
     return (
         <main className="Main">
