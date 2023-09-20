88d885a83013dadfa2d3cf528a88d81f73f3d680
Brian Bunker <bbunk001@ucr.edu>
2023-09-20 15:40:44 -0700

    modified:   src/App.css 	modified:   src/Specials.js

--- capstone/src/Specials.js
+++ capstone/src/Specials.js
@@ -1,7 +1,52 @@
 function Specials() {
     return (
-        <>
-        </>
+        <section className="Specials">
+            <div className="SpecialsTopWrapper">
+                <h2 className="SpecialsPromoText"></h2>
+                <button className="stdButton OnlineOrderBtn">Online Menu</button>
+            </div>
+            <div className="SpecialCardWrapper">
+                <div className="Card">
+                    <img src = "/public/bruchetta.svg"></img>
+                    <div className="CardTopLine">
+                        <h3 className="dishName">Greek salad</h3>
+                        <h4 className="dishPrice">$12.99</h4>
+                    </div>
+                    <p className="dishDescription">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                    <div className="CardBotLine">
+                        <h4 className="orderDelivery">Order a delivery</h4>
+                        <h4 className="deliveryIcon"></h4>
+                    </div>
+                </div>
+                
+                <div className="Card">
+                    <img src = "/public/bruchetta.svg"></img>
+                    <div className="CardTopLine">
+                        <h3 className="dishName">Greek salad</h3>
+                        <h4 className="dishPrice">$12.99</h4>
+                    </div>
+                    <p className="dishDescription">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                    <div className="CardBotLine">
+                        <h4 className="orderDelivery">Order a delivery</h4>
+                        <h4 className="deliveryIcon"></h4>
+                    </div>
+                </div>
+                
+                <div className="Card">
+                    <img src = "/public/bruchetta.svg"></img>
+                    <div className="CardTopLine">
+                        <h3 className="dishName">Greek salad</h3>
+                        <h4 className="dishPrice">$12.99</h4>
+                    </div>
+                    <p className="dishDescription">The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+                    <div className="CardBotLine">
+                        <h4 className="orderDelivery">Order a delivery</h4>
+                        <h4 className="deliveryIcon"></h4>
+                    </div>
+                </div>
+
+            </div>
+        </section>
     )    
 }
 
