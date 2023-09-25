e01c7def596e579bdf81313d80ed2f374899a311
Brian Bunker <bbunk001@ucr.edu>
2023-09-25 14:14:10 -0700

    modified:   src/App.css 	modified:   src/Testimonials.js 	public/greekSalad.jpg

--- capstone/src/Testimonials.js
+++ capstone/src/Testimonials.js
@@ -1,7 +1,42 @@
 function Testimonials() {
     return (
-        <>
-        </>
+        <section class="Testimonials">
+            <h3>Testimonials</h3>
+            <div class="TestWrapper">
+                <div class="TestCard">
+                    <h4>Rating</h4>
+                    <div class="AvatarName">
+                        <img class="Avatar"></img>
+                        <h4>Name</h4>
+                    </div>
+                    <p>Review Text</p>
+                </div>
+                <div class="TestCard">
+                    <h4>Rating</h4>
+                    <div class="AvatarName">
+                        <img class="Avatar"></img>
+                        <h4>Name</h4>
+                    </div>
+                    <p>Review Text</p>
+                </div>
+                <div class="TestCard">
+                    <h4>Rating</h4>
+                    <div class="AvatarName">
+                        <img class="Avatar"></img>
+                        <h4>Name</h4>
+                    </div>
+                    <p>Review Text</p>
+                </div>
+                <div class="TestCard">
+                    <h4>Rating</h4>
+                    <div class="AvatarName">
+                        <img class="Avatar"></img>
+                        <h4>Name</h4>
+                    </div>
+                    <p>Review Text</p>
+                </div>
+            </div>
+        </section>
     )    
 }
 
